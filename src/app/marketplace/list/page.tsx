1079878229ce03369d153821cc28f203a6e42776
Hariz Hakim <harizhakim85@gmail.com>
2025-03-13 21:05:17 +0800

    update claim

--- src/app/marketplace/list/page.tsx
+++ src/app/marketplace/list/page.tsx
@@ -39,7 +39,7 @@ export default function ListNFTPage() {
   const [nfcSupported, setNfcSupported] = useState<boolean | null>(null);
   const [isScanning, setIsScanning] = useState(false);
   const [scannedNFT, setScannedNFT] = useState<number | null>(null);
-  const requireNfcVerification = true; //testing purposes
+  const requireNfcVerification = false; //testing purposes
   const router = useRouter();
   
   // Use reown wallet integration
